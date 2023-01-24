dc0026e2af16db76aa4e7e9cc22ae492b6a9e59a
Joshua Martinez <JoshuaRM415@gmail.com>
2023-01-24 15:39:41 -0800

    change port number

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 
 //code to define port number. Im not so sure about how this works.
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 80;
 
 app.use(errorHandler());
 
