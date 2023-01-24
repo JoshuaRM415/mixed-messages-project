d78464034837aa35afb0d8308ad40193c0d0a923
Joshua Martinez <JoshuaRM415@gmail.com>
2023-01-24 15:08:16 -0800

    install dependencies, add a css file, reorganize the project directory.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const errorHandler = require('errorhandler')
+
+
+app.use(express.static('/public'))
+app.use(morgan('short'));
+app.use(bodyParser.json());
+
+
+//code to define port number. Im not so sure about how this works.
+const PORT = process.env.PORT || 4000;
+
+app.use(errorHandler());
+
+app.listen(PORT,()=>{
+    console.log(`server is listening on port ${PORT}`)
+});
+
--- public/resources/javascript/interactivity.js
+++ public/resources/javascript/interactivity.js
@@ -4,4 +4,3 @@
 const apiKey = 'cK3MO4hHN6xXMkeA0TK2g5LJuXYJxxCD';
 const baseUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 
-fetch(baseUrl) 
